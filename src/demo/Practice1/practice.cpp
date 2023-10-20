bf745488c8839015fa355288135455d0b5769bb6
DDY <cccscccs111@gmail.com>
2023-10-20 22:55:47 +0800

    cmake的第一个练习

---
+++ src/demo/Practice1/practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "PracticeConfig.h"
+// using namespace std;
+
+int main(int argc, char *argv[]){
+    // if(argc < 2){
+        std::cout << "Version: "<< Practice1_VERSION_MAJOR<< "." << Practice1_VERSION_MINOR << std::endl;
+        return 1;
+    // }    
+}
---
+++ src/demo/Practice1_build/PracticeConfig.h
@@ -0,0 +1,2 @@
+#define Practice1_VERSION_MAJOR 1
+#define Practice1_VERSION_MINOR 0
