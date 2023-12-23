6b39042a5ad9b3fdfc420e9c1ac4a0a64e0a2b61
DDY <cccscccs111@gmail.com>
2023-12-23 12:52:54 +0800

    预编译条件定义

--- src/demo/Library/MathFunctions/mysqrt.cpp
+++ src/demo/Library/MathFunctions/mysqrt.cpp
@@ -6,15 +6,20 @@
 double mysqrt(double num)
 {
 
-  std::cout << "\r \n the num is " << num <<std::endl;
+  std::cout << "\n the num is " << num << std::endl;
+
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  std::cout << "we are using log and exp calcualte the num sqrt for result:" << exp(log(num)/2) << std::endl;
+#endif
 
   double x = num;
-    double root;
-    
-    for(int i = 0; i < 10; i++) {
-        root = 0.5 * (x + (num / x));
-        x = root;
-    }
-    
-    return root;
+  double root;
+
+  for (int i = 0; i < 10; i++)
+  {
+    root = 0.5 * (x + (num / x));
+    x = root;
+  }
+
+  return root;
 }
